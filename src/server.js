a3d0c98399b008cb585fe02c210567b437e74fb6
Андрій <gapeev1979@gmail.com>
2025-09-26 18:14:08 +0300

    again fix error

--- src/server.js
+++ src/server.js
@@ -51,10 +51,11 @@ app.use((req, res) => {
 
 // помилки
 app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
+  // console.error('Error:', err.message);
   res.status(500).json({
+    message: err.message,
     // message: 'Simulated server error.',
-    error: err.message,
+    // error: err.message,
   });
 });
 
